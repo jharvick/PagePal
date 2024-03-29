7c4fdab16368c95eacb4d4a663cc855bd5482950
Jared Harvick <jharvick38@gmail.com>
2024-03-29 12:02:38 -0500

    Added create action

--- test/controllers/items_controller_test.rb
+++ test/controllers/items_controller_test.rb
@@ -10,4 +10,13 @@ class ItemsControllerTest < ActionDispatch::IntegrationTest
     assert_equal Item.count, data.length
   end
 
+  test "create" do
+    assert_difference "Item.count", 1 do
+      post "/items.json",
+        params: { name: "Pariah", image_url: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1347352994i/13417141.jpg", description: "A culmination of the Inquisitor series", category: "Sci-Fi" }
+        # headers: { "Authorization" => "Bearer #{@jwt}" }
+      assert_response 200
+    end
+  end
+
 end
