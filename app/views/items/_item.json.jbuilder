526f59cb31766cb6bc19e2378f916957e77561be
Jared Harvick <jharvick38@gmail.com>
2024-03-29 11:55:56 -0500

    Added index action

---
+++ app/views/items/_item.json.jbuilder
@@ -0,0 +1,6 @@
+json.name item.name
+json.image_url item.image_url
+json.description item.description
+json.category item.category
+json.created_at item.created_at
+json.updated_at item.updated_at
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,8 +9,8 @@
 User.create(name: "Jared", email: "jared@test.com", password_digest: "password", image_url: "https://static.wikia.nocookie.net/p__/images/3/36/Farvarod_08.png/revision/latest?cb=20190728143717&path-prefix=protagonist")
 User.create(name: "Chloe", email: "chloe@test.com", password_digest: "password", image_url: "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F6%2F2018%2F02%2F2475_tp_00013r-2000.jpg")
 
-Item.create(name: "Dune", image_url: "https://covers.shakespeareandcompany.com/97814732/9781473233959.jpg", description: "A sci-fi epic", category: "Book")
-Item.create(name: "Ravenor Rogue", image_url: "https://i.ebayimg.com/images/g/x8sAAOSw1aNetxqd/s-l1600.jpg", description: "A thrilling battle against chaos", category: "Book")
+Item.create(name: "Dune", image_url: "https://covers.shakespeareandcompany.com/97814732/9781473233959.jpg", description: "A sci-fi epic", category: "Novel")
+Item.create(name: "Ravenor Rogue", image_url: "https://i.ebayimg.com/images/g/x8sAAOSw1aNetxqd/s-l1600.jpg", description: "A thrilling battle against chaos", category: "Novel")
 
 Favorite.create(user_id: 1, item_id: 1)
 Favorite.create(user_id: 2, item_id: 2)
